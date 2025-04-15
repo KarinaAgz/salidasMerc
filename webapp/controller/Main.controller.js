ddf8a99204026a1c823b1f0b28f09b4c7bee40fe
KarinaAgz <karinaangon10@gmail.com>
2025-04-15 19:17:33 +0000

    cambios en la vits1

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,50 +1,125 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast"
-], (Controller) => {
+], function (Controller, MessageToast) {
     "use strict";
 
     return Controller.extend("logaligroup.mapeobapi.controller.Main", {
-        onInit() {
-            this.getView().setModel(this.getOwnerComponent().getModel());
+        onInit: function () {
+            var oModel = new sap.ui.model.json.JSONModel({
+                header: {
+                    reference_type: "",
+                    reserv_no: "",
+                    res_item: "",
+                    orderid: "",
+                    move_type: "",
+                    pstng_date: new Date().toISOString().split("T")[0],
+                    doc_date: new Date().toISOString().split("T")[0],
+                    ref_doc_no: "",
+                    header_txt: "",
+                    ver_gr_gi_slip: "3", // Default value
+                    ver_gr_gi_slipx: "X" // Default value
+                },
+                code: {
+                    gm_code: "03" // Default value for Goods Issue
+                },
+                items: [], // Array para las posiciones
+                currentItem: {
+                    material: "",
+                    plant: "",
+                    stge_loc: "",
+                    batch: "",
+                    entry_qnt: "",
+                    entry_uom: "",
+                    costcenter: "",
+                    orderid: "",
+                    move_reas: ""
+                }
+            });
+            this.getView().setModel(oModel, "mainModel");
         },
 
-        onselectionchange:function(oEvent){
-            var sReferenceType=oEvent.getSource().getSelectedKey();
-            this.getView().getModel().setProperty("/header/reference_type", sReferenceType);
-
-            //Limpiar campos condicionales
-            this.getView().getModel().setProperty("/header/reserv_no","");
-            this.getView().getModel().setProperty("header/res_item","");
-            this.getView().getModel().setProperty("/header/orderid","");
-            this.getView().getModel().setProperty("/header/move_type","");
+        onSelectionChange: function (oEvent) {
+            var sSelectedKey = oEvent.getSource().getSelectedKey();
+            var oModel = this.getView().getModel("mainModel");
+            if (oModel) {
+                oModel.setProperty("/header/reference_type", sSelectedKey);
+                MessageToast.show("Seleccionaste: " + sSelectedKey);
+                this._clearRequiredFieldStyles();
+            } else {
+                MessageToast.show("Error: Modelo no encontrado");
+            }
         },
 
-        onNext:function(){
-            var oModel=this.getView().getModel();
-            var oHeader=oModel.getProperty("/header");
-
-            //validaciones
-
-            if(!oHeader.sReference_Type){
-                MessageToast.show("Seleccione un tipo de referencia");
+        onNext: function () {
+            var oModel = this.getView().getModel("mainModel");
+            if (!oModel) {
+                MessageToast.show("Error: Modelo no encontrado");
                 return;
             }
-            if(oHeader.reference_type ==="reserva"){
-                if(!oHeader.reserv_no){
-                    MessageToast.show("complete el numero de reserva");
-                    return;
+
+            var oHeader = oModel.getProperty("/header");
+            if (!oHeader) {
+                MessageToast.show("Error: Datos del header no encontrados");
+                return;
+            }
+
+            this._clearRequiredFieldStyles();
+
+            var bHasErrors = false;
+
+            if (!oHeader.reference_type) {
+                MessageToast.show("Por favor, selecciona un tipo de referencia");
+                this.byId("opcionesSelect").addStyleClass("requiredFieldEmpty");
+                bHasErrors = true;
+            }
+
+            if (oHeader.reference_type === "reserva") {
+                if (!oHeader.reserv_no) {
+                    MessageToast.show("Por favor, completa el número de reserva");
+                    this.byId("reservNo").addStyleClass("requiredFieldEmpty");
+                    bHasErrors = true;
+                }
+                if (!oHeader.res_item) {
+                    MessageToast.show("Por favor, completa la posición de reserva");
+                    this.byId("resItem").addStyleClass("requiredFieldEmpty");
+                    bHasErrors = true;
                 }
             }
-            if(oHeader.reference_type=== "orden" && !oHeader.orderid){
-                MessageToast.show("complete el numero de orden");
-                return;
-            
-            }
-            //NAvegar a posiciones
-            this.getOwnerComponent().getRouter().navTo("item");
 
+            if (oHeader.reference_type === "orden" && !oHeader.orderid) {
+                MessageToast.show("Por favor, ingresa el número de orden");
+                this.byId("orderId").addStyleClass("requiredFieldEmpty");
+                bHasErrors = true;
+            }
+
+            if (oHeader.reference_type === "otros" && !oHeader.move_type) {
+                MessageToast.show("Por favor, selecciona una clase de movimiento");
+                this.byId("claseMovimiento").addStyleClass("requiredFieldEmpty");
+                bHasErrors = true;
+            }
+
+            if (bHasErrors) {
+                return;
+            }
+
+            this.getOwnerComponent().getRouter().navTo("item");
+        },
+
+        _clearRequiredFieldStyles: function () {
+            var aFields = [
+                "opcionesSelect",
+                "reservNo",
+                "resItem",
+                "orderId",
+                "claseMovimiento"
+            ];
+            aFields.forEach(function (sFieldId) {
+                var oField = this.byId(sFieldId);
+                if (oField) {
+                    oField.removeStyleClass("requiredFieldEmpty");
+                }
+            }.bind(this));
         }
-    
     });
 });
